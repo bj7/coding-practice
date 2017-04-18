36f7818663137344d3e820bd483c6af4e6e06eb0
Josh Bernitt <jjbernitt@gmail.com>
2017-04-18 18:20:50 -0400

    Initial commit

---
+++ wordLadder.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var beginWord = "hit";
+var endWord = "cog";
+var wordList = ["hot",
+                "dot",
+                "dog",
+                "lot",
+                "log",
+                "cog"];
+
+func wordLadder(beginWord: String, endWord: String, wordList: Array<String>) -> Int {
+    var Q = [];
+    var S = [];
+}
+
+
+
+
+wordLadder(beginWord: beginWord, endWord: endWord, wordList: wordList);
